7f7969daf7bedb402b131d3d709fcbdc0ff8b321
Adrien <A.pats@hotmail.fr>
2024-08-05 16:15:30 +0200

    exo JS

---
+++ 03-Javascript/07-fonctions/Exercices/exo-1.js
@@ -0,0 +1,12 @@
+// Exercice 1 :
+//  Créez une fonction normal qui prend un nombre en entrée et renvoie son carré
+// function nombre (a) { // avec arguments
+//     result = a * a;
+//     return result; //return oblogatoire
+// };
+// Transformez cette fonction en une fonction fléchée
+const nombre = (a) => {
+    return a * a;
+};
+// résultat attendu :
+console.log(nombre(5)); // => 25
---
+++ 03-Javascript/07-fonctions/function_flechee_plus.js
@@ -0,0 +1,17 @@
+//  ES6 => function fléchée
+
+const direBonjour = (prenom) => {
+    console.log(`Bonjour ${prenom}`);
+};
+// Si un seul argument => parenthèses facultatives
+// Si une seule instruction => accolades facultatives
+// const direBonjour = prenom => console.log(`Bonjour ${prenom}`);
+
+direBonjour('Adrien');
+
+
+if (true) {
+    console.log("vrai");
+};
+// if (true) console.log("vrai");
+
