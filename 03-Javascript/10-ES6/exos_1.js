7e15a2eb06e1ff58f318af9e0d9a76e246a97052
Adrien <A.pats@hotmail.fr>
2024-08-08 10:44:50 +0200

    branch dev jeudi

---
+++ 03-Javascript/10-ES6/exos_1.js
@@ -0,0 +1,9 @@
+// créer quelques objets "voiture" (mini 3) => 2 attributs : marque et puissance
+// mettre ces objets dans un tableau
+// créer une fonction qui prends en paramètre le tableau et une marque recherchée
+// => nous renvoies le nombre de voitures trouvées
+// Obligation d'utiliser uniquement la destructuration et le spread opérator
+
+rechercheParMarque('renault', ??) // => nombre de voiture trouvées
+
+// 3 fois (...)
