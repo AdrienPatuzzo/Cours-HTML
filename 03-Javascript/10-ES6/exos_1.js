27dc2d8a714ee374c1c67880b70f481e8ba6d769
Adrien <A.pats@hotmail.fr>
2024-08-08 12:16:59 +0200

    cours du jeudi

--- 03-Javascript/10-ES6/exos_1.js
+++ 03-Javascript/10-ES6/exos_1.js
@@ -1,9 +1,38 @@
+#!/usr/bin/env node
 // créer quelques objets "voiture" (mini 3) => 2 attributs : marque et puissance
 // mettre ces objets dans un tableau
 // créer une fonction qui prends en paramètre le tableau et une marque recherchée
 // => nous renvoies le nombre de voitures trouvées
 // Obligation d'utiliser uniquement la destructuration et le spread opérator
-
-rechercheParMarque('renault', ??) // => nombre de voiture trouvées
-
+// => nombre de voiture trouvées
 // 3 fois (...)
+
+const voiture = {
+    marque: 'renault',
+    puissance: 'bien',
+}
+const voiture2 = {
+    marque: 'peugeot',
+    puissance: 'cool',
+}
+const voiture3 = {
+    marque: 'toyota',
+    puissance: 'tranquil',
+}
+let tabVoitures = [];
+// let tabVoitures = [voiture, voiture2, voiture3];
+tabVoitures = [...tabVoitures, voiture, voiture2, voiture3];
+
+function rechercheParMarque(marqueRecherchee, ...args){
+    let cpt = 0;
+    for (const voiture of args) {
+        const {marque} = voiture;
+        if (marqueRecherchee === marque){
+            cpt++;
+        }
+    }
+    // console.log(`il y a ${cpt} voitures`);
+    console.log(`${ cpt > 0 ? `il y a ` + cpt : "il n'y a aucune"} voiture de la marque ${marqueRecherchee}`);
+};
+
+rechercheParMarque('renault', ...tabVoitures)
---
+++ 03-Javascript/10-ES6/exos_2.js
@@ -0,0 +1,8 @@
+// Créer un tableau d'utilisateurs vide
+// y stocker 2 ou plus utilisateurs qui possèdent un "nom" et un "service" (ex: "informatique")
+// créer une fonction qui permets de changer le service d'un utilisateur en y passant son nom, le nouveau service
+// et le tableau d'origine
+// cette fonction retourne un novueau tableau à jour
+// mettre à jour le tableau de base et l'afficher pour vérifier le changement
+// destructuration et spread operator
+// console.log du tableau d'origine modifié
