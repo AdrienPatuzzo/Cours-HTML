25f9f65c79b156a6801d0133bafe0452e44bef3e
Adrien <A.pats@hotmail.fr>
2024-09-25 16:13:13 +0200

    fin du début du cours PHP

---
+++ 06-php/dateFr.php
@@ -0,0 +1,33 @@
+<?php
+// Date en français
+$jour = getdate();
+print_r(getdate());
+$semaine = array(
+    " Dimanche ",
+    " Lundi ",
+    " Mardi ",
+    " Mercredi ",
+    " Jeudi ",
+    " vendredi ",
+    " samedi "
+);
+$mois = array(
+    1 => " janvier ",
+    " février ",
+    " mars ",
+    " avril ",
+    " mai ",
+    " juin ",
+    " juillet ",
+    " août ",
+    " septembre ",
+    " octobre ",
+    " novembre ",
+    " décembre "
+);
+print_r($mois);
+// Avec getdate()
+echo "Français : Avec getdate() : Aujourd'hui ", $semaine[$jour['wday']], $jour['mday'], $mois[$jour['mon']], $jour['year'], "\n";
+// Avec date()
+echo "Français : Avec date() : Aujourd'hui ",
+$semaine[date('w')], " ", date('j'), "", $mois[date('n')], date('Y'), "\n";
---
+++ 06-php/dateFrBis.php
@@ -0,0 +1,6 @@
+<?php
+
+$formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG,
+IntlDateFormatter::NONE);
+$dateFr = $formatter->format(time());
+echo $dateFr;
